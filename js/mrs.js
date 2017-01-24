1f0b0be06799666a046d3679147803c6f672fd9a
Bernardo Covas <bernardo.covas@hotmail.com>
2017-01-24 16:13:46 +0000

    #

--- js/mrs.js
+++ js/mrs.js
@@ -2,18 +2,21 @@
 $(document).ready(function()
 {
 
-  $("#home-header").load("/html/elements/header.html", function()
-  {
-    $(".header-container").height($(window).height());
-    $(".header-container").width($(window).width());
-  });
-
+  $("#home-header").load("/html/elements/header.html");
 
   $("#nav").load("/html/elements/nav.html");
 
   $("#page").load("/html/views/description.html");
 
-  $("#team").load("/html/elements/team.html");
+  $("#team").load("/html/views/team.html");
+
+  $("#projects").load("/html/views/projects.html");
+
+  $("#debug").click(function()
+  {
+    console.log("clicked");
+    $(".debug").css("border", "1px solid red");
+  });
 
 });
 
@@ -21,12 +24,16 @@ $(window).resize(function()
 {
   $(".header-container").height($(window).height());
   $(".header-container").width($(window).width());
+
+  if($(window).scrollTop() >= $(window).height())
+  {
+    $("#nav").css("top", "0");
+  }
+  else
+    $("#nav").css("top", $(window).height() - $(window).scrollTop());
+
 });
 
-$("#debug").click(function()
-{
-  $(".debug").css("border", "1px solid red");
-});
 
 $(window).scroll(function()
 {
